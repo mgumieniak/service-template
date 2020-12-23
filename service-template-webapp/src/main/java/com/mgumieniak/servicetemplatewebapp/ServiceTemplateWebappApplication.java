999cf7b29652984727c4a8342f128ee57adc4d51
mgumieniak <maciejgumieniak007@gmail.com>
2020-12-23 19:59:04 +0100

    Init commit

---
+++ service-template-webapp/src/main/java/com/mgumieniak/servicetemplatewebapp/ServiceTemplateWebappApplication.java
@@ -0,0 +1,13 @@
+package com.mgumieniak.servicetemplatewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTemplateWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTemplateWebappApplication.class, args);
+    }
+
+}
